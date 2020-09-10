c4351e17dee6a3b41bcfcd578e32b46caedfdc39
chinhhi102 <chinhhi102@gmail.com>
2020-09-10 16:32:08 +0700

    update web

--- Exercise_4.py
+++ Exercise_4.py
@@ -34,7 +34,7 @@ def post_something():
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
+            "Message": f"Welcome Công Chính to Công Chính awesome platform!!",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
         })
