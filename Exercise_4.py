3060cfd6b0b7aed470366c5b4e264f7c7e85372f
chinhhi102 <chinhhi102@gmail.com>
2020-09-10 15:12:44 +0700

    update index

--- Exercise_4.py
+++ Exercise_4.py
@@ -47,7 +47,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to our server của Chính!!</h1>"
 
 
 if __name__ == '__main__':
